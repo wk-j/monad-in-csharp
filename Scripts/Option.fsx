ee614a74b99ef0b2ac50736f06f3f15b9c2f0ea9
wk-j <somnuk.wk@gmail.com>
2017-11-21 01:38:49 +0700

    Converted fun to Func

--- Scripts/Option.fsx
+++ Scripts/Option.fsx
@@ -1,27 +1,30 @@
 open System
 
-type 't Options  = { 
-    Value:  't
+type 'target Options  = { 
+    Value:  'target
     HasValue: bool
  } with
-    static member None = { HasValue = false ; Value = Unchecked.defaultof<'t> }
-    static member Some<'t> (t: 't) = { HasValue = true ; Value = t }
+    static member None = { HasValue = false ; Value = Unchecked.defaultof<'target> }
+    static member Some (t: 'target) = { HasValue = true ; Value = t }
+    static member Unit(t: 'target) = Options.Some(t)
 
 [<Runtime.CompilerServices.Extension>]
 type Extensions =
-    [<Runtime.CompilerServices.Extension>]
-    static member Unit(_: 'target Options, t: 'target) = Options.Some<'target>(t)
 
     [<Runtime.CompilerServices.Extension>]
-    static member Bind(target: 'target Options, action: 'target  -> 'result Options) = 
+    static member Bind(target: 'target Options, action: Func<'target,'result Options>) = 
         if target.HasValue then
-            action(target.Value)
+            action.Invoke(target.Value)
         else
             Options.None
 
     [<Runtime.CompilerServices.Extension>]
-    static member SelectMany(target: 'source Options, projector: 'source -> 'result Options) = 
+    static member SelectMany(target: 'source Options, projector: Func<'source,'result Options>) = 
         target.Bind(projector)
+
+    [<Runtime.CompilerServices.Extension>]
+    static member SelectMany(target: 'source Options, projector: Func<'source,'result Options>, selector: Func<'source,'option,'target>) = 
+        target.Bind(fun source -> projector.Invoke(source).Bind(fun task -> Options.Unit(selector.Invoke(source, task))))
 
 let parseIntOption(str) =
     let ok,rs = Int32.TryParse(str)
